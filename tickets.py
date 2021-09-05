681e0895a5602c346d3c2863f334034f153c6ad5
Dont-Panic42 <88267502+Dont-Panic42@users.noreply.github.com>
2021-09-05 19:54:26 +0800

    A new file adder

--- tickets.py
+++ tickets.py
@@ -1,24 +1,24 @@
 tickets = int(input("Введите количество билетов: "))
-total = 0
+result = 0
 count = 0
 
 for i in range(tickets):
     age = int(input("Введите возраст посетителя: "))
     if age < 18:
         print("Вход бесплатный")
-        total += 0
+        result += 0
         count += 1
     elif 18 <= age < 25:
         print("Стоимость билета: 990 рублей")
-        total += 990
+        result += 990
         count += 1
     else:
         print("Стоимость билета: 1390 рублей")
-        total += 1390
+        result += 1390
         count += 1
 
-print(f"Общая стоимость билетов {count} билетов: {total} рублей")
+print(f"Общая стоимость билетов {count} билетов: {result} рублей")
 
 if tickets > 3 and count > 3:
-    total = total - total * 0.1
-    print(f"Стоимость {count} билетов со скидкой 10%: {total} рублей")
+    result = result - result * 0.1
+    print(f"Стоимость {count} билетов со скидкой 10%: {result} рублей")
